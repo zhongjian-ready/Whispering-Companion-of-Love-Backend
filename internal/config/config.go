561469546c461b5f67a1e60dc5908a1453e6af94
ZhongJian <zhongjian.mail.2014@gmail.com>
2025-12-23 11:30:30 +0800

    feat: reset docker

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"strings"
+
 	"github.com/spf13/viper"
 )
 
@@ -36,6 +38,7 @@ func LoadConfig() (*Config, error) {
 	viper.SetConfigName("config")
 	viper.SetConfigType("yaml")
 
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
 	viper.AutomaticEnv()
 
 	if err := viper.ReadInConfig(); err != nil {
